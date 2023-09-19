7fd8e7c105fdbf8fe32f611d98ef67f2f6368e92
ayush <ayush.tiwari@staqo.com>
2023-09-19 17:58:57 +0530

    add::atless

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,11 @@
 // getting-started.js
 const mongoose = require('mongoose');
 const db = mongoose.connection
-
+const uri = "mongodb+srv://iamayushtiwari:uaIcbCh95wbXQPCi@cluster0.z5fgwzr.mongodb.net/?retryWrites=true&w=majority";
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/chatroom_development');
+  await mongoose.connect(uri);
   // console.log("database loaded")
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
